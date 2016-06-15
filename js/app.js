ae811197f798ff36f8bad3b06e41032612276fc9
Joe Seago <joe.seago@gmail.com>
2016-06-15 13:55:01 -0600

    newGame function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,41 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	//newGame function
+	$(".new").click(function() {
+		newGame ();
+	});
+
+
+	var secretNumber
+	var Guess
+	var count = 0
+
+
+	function newGame() {
+
+	};
+
+
+
+
+
+
+
+
+
+  	//create secret number function
+  	//
+  	//user guess function
+  	//
+  	//tracking number of guesses function
+  	//
+  	//check for numeric input between 1 and 100
+  	//
+  	//start newGame function
+  	//
+  	//OPTIONAL warmer/colder
+
 });
 
 
