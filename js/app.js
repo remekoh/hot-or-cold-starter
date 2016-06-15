f8c51c677b5e09d5253de36e9f81bf4bfc46a5ce
rem{e}koh <git@remekoh.me>
2016-06-15 16:36:18 -0500

    created 2 functions

--- js/app.js
+++ js/app.js
@@ -1,52 +1,69 @@
+var secretNumber;
+var count = 0;
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+function newGame() {
+    count = 0;
+    $("h2").text('Make your Guess!');
+    $("#count").text("0");
+    $('#userGuess').val('');
+    $("#guessList").empty();
+    secretNumber = getRandom(1, 101);
+    // console.log(secretNumber);
+};
 
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
-  	//newGame function
-	$(".new").click(function() {
-		newGame ();
-	});
-
-
-	var secretNumber
-	var Guess
-	var count = 0
+function getRandom(min, max) {
+    // var num =
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
-	function newGame() {
+$(document).ready(function() {
 
-	};
+    /*--- Display information modal box ---*/
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
+
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
+
+    //newGame function
+    $(".new").click(function() {
+        newGame();
+    });
+
+    $('#game-form').submit(function(e) {
+        e.preventDefault();
+        var guess = $('#userGuess').val();
+        $('#userGuess').val('');
+        // console.log(guess);
+        count++;
+        $('#count').text(count);
+    });
 
 
-
-
-
-
-
-
-
-  	//create secret number function
-  	//
-  	//user guess function
-  	//
-  	//tracking number of guesses function
-  	//
-  	//check for numeric input between 1 and 100
-  	//
-  	//start newGame function
-  	//
-  	//OPTIONAL warmer/colder
 
 });
 
 
+
+
+
+
+
+
+
+//create secret number function
+//
+//user guess function
+//
+//tracking number of guesses function
+//
+//check for numeric input between 1 and 100
+//
+//start newGame function
+//
+//OPTIONAL warmer/colder
