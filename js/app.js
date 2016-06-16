f892cafce21ed81970d20df034414f7a4115b0e4
rem{e}koh <git@remekoh.me>
2016-06-16 00:03:32 -0500

    created alert messages and displayed guesses

--- js/app.js
+++ js/app.js
@@ -1,22 +1,6 @@
 var secretNumber;
 var count = 0;
 
-function newGame() {
-    count = 0;
-    $("h2").text('Make your Guess!');
-    $("#count").text("0");
-    $('#userGuess').val('');
-    $("#guessList").empty();
-    secretNumber = getRandom(1, 101);
-    // console.log(secretNumber);
-};
-
-function getRandom(min, max) {
-    // var num =
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
 $(document).ready(function() {
 
     /*--- Display information modal box ---*/
@@ -30,7 +14,7 @@ $(document).ready(function() {
         $(".overlay").fadeOut(1000);
     });
 
-    //newGame function
+    //Start new game
     $(".new").click(function() {
         newGame();
     });
@@ -39,14 +23,40 @@ $(document).ready(function() {
         e.preventDefault();
         var guess = $('#userGuess').val();
         $('#userGuess').val('');
-        // console.log(guess);
         count++;
         $('#count').text(count);
+        if (guess > 100 || guess < 0) {
+            $("#game-form")[0].reset();
+            alert("enter a number between 1 and 100");
+        }
+        if (isNaN(guess)) {
+            $("#game-form")[0].reset();
+            alert("please enter a number");
+        }
+        if (guess !== "") {
+            var entry = "<li>"+guess+"</li>"
+            $("#guessList").append(entry);
+        }
+
     });
 
-
-
 });
+
+//newGame function
+function newGame() {
+    count = 0;
+    $("h2").text('Make your Guess!');
+    $("#count").text("0");
+    $('#userGuess').val('');
+    $("#guessList").empty();
+    secretNumber = getRandom(1, 101);
+    // console.log(secretNumber);
+};
+
+function getRandom(min, max) {
+    // var num =
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 
 
